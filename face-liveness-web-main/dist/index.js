0afbd88383242c4ce371d9d4570f376a5dd00ba6
Makoshaa <103.stalker.103@gmail.com>
2025-10-19 16:43:51 +0500

    Initial commit

--- face-liveness-web-main/dist/index.js
+++
@@ -1,6 +0,0 @@
-import './globals.d.ts';
-import { captureAndSendSnapshot } from './main';
-// Expose for the button onclick
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-;
-window.captureAndSendSnapshot = captureAndSendSnapshot;
